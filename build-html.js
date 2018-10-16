9336ea3e21972a7742504bad4ba3808db0b00be2
Jack <fallows.jack@gmail.com>
2018-10-16 17:36:58 +0100

    fixed output html tab indentation + dependencies correctly formatted

--- build-html.js
+++ build-html.js
@@ -2,35 +2,35 @@ function buildHtml(dependencies, testsSource, htmlBody) {
     return `
 <html>
 <head>
-  <meta charset="utf-8">
+    <meta charset="utf-8">
 </head>
 <body>
-  ${htmlBody || ""}
+    ${htmlBody || ""}
 
-  <script src="node_modules/qunit/qunit/qunit.js"></script>
+    <script src="node_modules/qunit/qunit/qunit.js"></script>
 
-  <script>
-    window.results = [];
-
-    QUnit.config.autostart = false;
-  </script>
-  ${dependencies.map(d => `<script src="${d}"></script>`)}
-  ${`<script src="${testsSource}"></script>`}
-  <script>
-    function runTests() {
-      return new Promise(resolve => {
-        QUnit.testDone(function(testResult) {
-            window.results.push(testResult);
-        });
-          
-        QUnit.done(function (overall) {
-          resolve({ overall, results: window.results });
-        });
-          
-        QUnit.start();
-      })
-    }
-  </script>
+    <script>
+        window.results = [];
+    
+        QUnit.config.autostart = false;
+    </script>
+    ${dependencies.map(d => `<script src="${d}"></script>`).join("\n\t")}
+    ${`<script src="${testsSource}"></script>`}
+    <script>
+        function runTests() {
+          return new Promise(resolve => {
+            QUnit.testDone(function(testResult) {
+                window.results.push(testResult);
+            });
+              
+            QUnit.done(function (overall) {
+              resolve({ overall, results: window.results });
+            });
+              
+            QUnit.start();
+          })
+        }
+    </script>
 </body>
 </html>
     `;
